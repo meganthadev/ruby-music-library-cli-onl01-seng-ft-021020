a9c2a45050b25fc482db05dfe42a1c4dd1bdad98
Megan Horsfield <meganthadev@gmail.com>
2020-03-07 06:17:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
-  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
   extend Concerns::Findable
+
+  attr_accessor :name
+  attr_reader :artist, :genre
 
   @@all = []
 
-  def initialize(name, artist=nil, genre=nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist=(artist) if artist != nil
-    self.genre=(genre) if genre != nil
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def self.all
@@ -22,14 +24,8 @@ class Song
     @@all << self
   end
 
-  def self.create(song)
-    song = self.new(song)
-    song.save
-    song
-  end
-
-  def artist
-    @artist
+  def self.create(name)
+    new(name).tap{ |song| song.save }
   end
 
   def artist=(artist)
@@ -37,48 +33,23 @@ class Song
     artist.add_song(self)
   end
 
-  def genre
-    @genre
-  end
-
   def genre=(genre)
     @genre = genre
     genre.songs << self unless genre.songs.include?(self)
   end
 
-  def self.find_by_name(name)
-    @@all.detect do |song|
-      song.name == name
-    end
-  end
-
-  def self.find_or_create_by_name(name)
-     @@all.detect do |song|
-       if song.name == name
-         song
-       else
-         self.create(name)
-       end
-     end
-    self.find_by_name(name) || self.create(name)
-
-  end
-
   def self.new_from_filename(filename)
-    array = filename.split(" - ")
-
-    song_name = array[1]
-    artist_name = array[0]
-    genre_name = array[2].split(".mp3").join
+    parts = filename.split(" - ")
+    artist_name, song_name, genre_name = parts[0], parts[1], parts[2].split(".mp3").join(" ")
 
     artist = Artist.find_or_create_by_name(artist_name)
     genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
+
+    Song.new(song_name, artist, genre)
   end
 
   def self.create_from_filename(filename)
-    self.new_from_filename(filename).save
+    new_from_filename(filename).tap{ |s| s.save }
   end
-
 
 end
